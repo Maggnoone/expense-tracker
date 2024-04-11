d98a2735b04406172461b5f8dfc01bafd6e59530
Mauricio Garcia <78531537+Maggnoone@users.noreply.github.com>
2024-04-11 15:52:54 -0400

    Fix styles in stats section

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -20,41 +20,50 @@ export const Stats = ({transactions}) => {
 
   return (
     <div className="px-5 bg-white pb-2">
-            <div className="flex">
-              <div className="flex-1 group">
-                <a
-                  href="#"
-                  className="flex items-end justify-center text-center mx-auto px-4 w-3/5 text-green-500 border-b-2 border-transparent border-green-500"
-                >
-                  <span className="block px-1">
-                    <h3>$ {sumProperties(incomes)}</h3>
-                    <span className="block text-xs pb-1">Incomes</span>
-                  </span>
-                </a>
-              </div>
-              <div className="flex-1 group">
-                <a
-                  href="#"
-                  className="flex items-end justify-center text-center mx-auto px-4 w-3/5 text-red-600 border-b-2 border-transparent border-red-600"
-                >
-                  <span className="block px-1">
-                    <h3>$ {sumProperties(expenses)}</h3>
-                    <span className="block text-xs pb-1">Expenses</span>
-                  </span>
-                </a>
-              </div>
-              <div className="flex-1 group">
-                <a
-                  href="#"
-                  className="flex items-end justify-center text-center mx-auto px-4 w-3/5 text-gray-600 border-b-2 border-transparent border-gray-500"
-                >
-                  <span className="block px-1">
-                    <h3>$ {Math.abs(sumProperties(incomes) - sumProperties(expenses))}</h3>
-                    <span className="block text-xs pb-1">{sumProperties(incomes) - sumProperties(expenses) > 0 ? "Total saved" : "In Debt" }</span>
-                  </span>
-                </a>
-              </div>
-            </div>
-          </div>
-  )
+      <div className="flex">
+        <div className="flex-1 group">
+          <a
+            href="#"
+            id="incomes-border"
+            className="flex items-end justify-center text-center mx-auto px-4 w-3/5 text-green-500 border-b-2 border-transparent"
+          >
+            <span className="block px-1">
+              <h3>$ {sumProperties(incomes)}</h3>
+              <span className="block text-xs pb-1">Incomes</span>
+            </span>
+          </a>
+        </div>
+        <div className="flex-1 group">
+          <a
+            href="#"
+            id="expenses-border"
+            className="flex items-end justify-center text-center mx-auto px-4 w-3/5 text-red-600 border-b-2 border-transparent"
+          >
+            <span className="block px-1">
+              <h3>$ {sumProperties(expenses)}</h3>
+              <span className="block text-xs pb-1">Expenses</span>
+            </span>
+          </a>
+        </div>
+        <div className="flex-1 group">
+          <a
+            href="#"
+            id="expenses-border"
+            className="flex items-end justify-center text-center mx-auto px-4 w-3/5 text-gray-600 border-b-2 border-transparent"
+          >
+            <span className="block px-1">
+              <h3>
+                $ {Math.abs(sumProperties(incomes) - sumProperties(expenses))}
+              </h3>
+              <span className="block text-xs pb-1">
+                {sumProperties(incomes) - sumProperties(expenses) > 0
+                  ? "Total saved"
+                  : "In Debt"}
+              </span>
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
